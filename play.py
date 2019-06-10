0a075b0f295a5a35ec4c79520bbf9a5d7014c0d9
xiaosiqi111 <2218085828@qq.com>
2019-06-10 23:29:48 +0800

    Update play.py

--- play.py
+++ play.py
@@ -13,7 +13,7 @@ def get_file_list(file_path):#返回按日期排序的文件列表
         # 注意，这里使用lambda表达式，将文件按照最后修改时间顺序升序排列
         # os.path.getmtime() 函数是获取文件最后修改时间
         # os.path.getctime() 函数是获取文件最后创建时间
-        dir_list = sorted(dir_list,  key=lambda x: os.path.getmtime(os.path.join(file_path, x)))
+        # dir_list = sorted(dir_list,  key=lambda x: os.path.getmtime(os.path.join(file_path, x)))
         # print(dir_list)
         return dir_list
 
